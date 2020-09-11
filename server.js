78ea260de5377e0704bab47493315537d5fe0001
Jong-Geon Lee <leejonggun@gmail.com>
2020-09-11 11:30:10 +0900

    using MySql  database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');   
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,7 +7,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+        host: conf.host,
+        user: conf.user,
+        password: conf.password,
+        port: conf.port,
+        database: conf.database
+    });
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
+
+
+    
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+    
+/*
     res.send([
         {
             'id': 1,
@@ -34,6 +60,10 @@ app.get('/api/customers', (req, res) => {
         }
     ]
     );
+*/
+
+
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
