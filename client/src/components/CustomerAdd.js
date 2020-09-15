dceb21b3fa4ccb7c52197c555c37235d713d6dc5
Jong-Geon Lee <leejonggun@gmail.com>
2020-09-15 22:48:54 +0900

    Add material desing for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { DialogContentText } from '@material-ui/core';
 
+
+const styles = theme => ({
+    hidden: {
+        display: 'none'
+    }
+
+});
 
 class CustomerAdd extends React.Component {
 
-    constructor(prop){
+    constructor(prop) {
         super(prop);
         this.state = {
             file: null,
             userName: '',
             birth: '',
-            gender:'',
-            job:'',
-            fileName: ''
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
         }
     }
 
@@ -47,7 +63,8 @@ class CustomerAdd extends React.Component {
             birth: '',
             gender: '',
             job: '',
-            fileName: ''
+            fileName: '',
+            open: false
         })
 
     }
@@ -63,22 +80,58 @@ class CustomerAdd extends React.Component {
     handleValueChange = (e) => {
         let nextState = {};
         nextState[e.target.name] = e.target.value;
-        this.setState(nextState); 
+        this.setState(nextState);
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            file: null,
+            userName: '',
+            birth: '',
+            gender: '',
+            job: '',
+            fileName: '',
+            open: false
+        })
     }
 
     render() {
 
-        return(
+        const { classes } = this.props;
 
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
-                생년월일: <input type="text" name="birth" value={this.state.birth} onChange={this.handleValueChange} /><br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
-                <button type="submit">추가 하기</button>
-            </form>
+        return (
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
+                    고객 추가하기
+                </Button>
+                <Dialog open={this.state.open} onClose={this.handleClose} >
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br />
+                        <label htmlFor="raised-button-file">
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br />
+                        <TextField label='이름' type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br />
+                        <TextField label='생년월일' type="text" name="birth" value={this.state.birth} onChange={this.handleValueChange} /><br />
+                        <TextField label='성별' type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br />
+                        <TextField label='직업' type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit} >추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose} >닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+
 
         )
 
@@ -87,4 +140,4 @@ class CustomerAdd extends React.Component {
 
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
